2062c51a15a411e17884b6291e5439c31465e0f8
kano <ajvls98@gmail.com>
2024-01-02 20:55:57 +0900

    route 수정

--- src/core/core.js
+++ src/core/core.js
@@ -22,7 +22,7 @@ function routeRender(routes) {
   if (!location.hash) {
     history.replaceState(null, '', '/#/') // (상태, 제목, 주소)
   }
-  const routerView = document.querySelector('router-view')
+  const routerView = document.querySelector('#router-view')
   const [hash, queryString = ''] = location.hash.split('?') // 물음표를 기준으로 해시 정보와 쿼리스트링을 구분
 
   // 1) 쿼리스트링을 객체로 변환해 히스토리의 상태에 저장!
