b1502c03e0007a4ef26e39c123a39be1af308b8c
kano <ajvls98@gmail.com>
2024-01-03 10:16:47 +0900

    Update image paths in SannabiScroll component

--- src/components/SannabiScroll.js
+++ src/components/SannabiScroll.js
@@ -4,23 +4,23 @@ import styles from './SannabiScroll.module.scss';
 
 export default class SannabiScroll extends Component {
 
-  render() {
-    this.el.innerHTML = /* html */`
+    render() {
+        this.el.innerHTML = /* html */`
     <div class="${styles.back}">
       <div id="rellax" class="${styles.mountain3} ${styles.rellax}" data-rellax-speed="4">
-          <img src="/src/img/Spr_PRLG_Forest_BG_Mountain.png" alt="">
+          <img src="/img/Spr_PRLG_Forest_BG_Mountain.png" alt="">
       </div>
       <div id="rellax" class="${styles.mountain4} ${styles.rellax}" data-rellax-speed="4">
-          <img src="/src/img/Spr_PRLG_Forest_BG_Mountain.png" alt="">
+          <img src="/img/Spr_PRLG_Forest_BG_Mountain.png" alt="">
       </div>
       <div id="rellax" class="${styles.cloud2} ${styles.rellax}" data-rellax-speed="-12">
-          <img src="/src/img/Spr_PRLG_Forest_BG_Cloud.png" alt="">
+          <img src="/img/Spr_PRLG_Forest_BG_Cloud.png" alt="">
       </div>
       <div id="rellax" class="${styles.mountain1} ${styles.rellax}" data-rellax-speed="-8">
-          <img src="/src/img/Spr_PRLG_Forest_BG_Mountain.png" alt="">
+          <img src="/img/Spr_PRLG_Forest_BG_Mountain.png" alt="">
       </div>
       <div class="${styles.mountain2} ${styles.rellax}" data-rellax-speed="-7">
-          <img src="/src/img/Spr_PRLG_Forest_BG_Mountain.png" alt="">
+          <img src="/img/Spr_PRLG_Forest_BG_Mountain.png" alt="">
       </div>
       <div id="rellax" class="${styles.tree1} ${styles.rellax}" data-rellax-speed="6">
           <img src="../img/Prop_Forest (78).png" alt="">
@@ -32,16 +32,16 @@ export default class SannabiScroll extends Component {
           <img src="Prop_Forest_sakura.png" alt="">
       </div>
       <div  class="${styles.ground}">
-          <img src="/src/img/ground.png" alt="">
+          <img src="/img/ground.png" alt="">
       </div>
       <div id="rellax" class="${styles.cloud1} ${styles.rellax}" data-rellax-speed="10">
-          <img src="/src/img/Spr_PRLG_Forest_BG_Cloud.png" alt="">
+          <img src="/img/Spr_PRLG_Forest_BG_Cloud.png" alt="">
       </div>
       <div id="rellax" class="${styles.cloud3} ${styles.rellax}" data-rellax-speed="1">
-          <img src="/src/img/Spr_PRLG_Forest_BG_Cloud.png" alt="">
+          <img src="/img/Spr_PRLG_Forest_BG_Cloud.png" alt="">
       </div>
   </div>
     `
 
-  }
+    }
 }
