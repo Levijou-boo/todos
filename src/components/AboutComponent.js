187131f7a1913fb23c48991082bbc432645dd5b9
kano <ajvls98@gmail.com>
2024-01-11 20:11:40 +0900

    Update Swiper configuration and styles

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -1,9 +1,12 @@
 import { Component } from "../core/core";
 import styles from "./AboutComponent.module.scss";
+// core version + navigation, pagination modules:
 import Swiper from 'swiper';
-import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
 
+// import Swiper and modules styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
 
 
 export default class AboutComponent extends Component {
@@ -50,41 +53,22 @@ export default class AboutComponent extends Component {
         </article>
         <div class=${styles["back2"]}>
         </div>
-        <article class=${styles["slider-wrapper"]}>
-        <div class="swiper mySwiper ${styles.swiper}">
-          <div class="swiper-wrapper ${styles["swiper-wrapper"]}">
-            <div class="swiper-slide ${styles["swiper-slide"]}">
-              <img src="/img/wfpXqAU6D45LO1jg8GGRXPXwJPPAuyY3JjTLsO7yXDKHeiPqyMuZPGNq-R8BFRCWzq75vhChjCvnux5XOGyIQ6a1JYEmthzIvVrpp7tkmyzH09dy8FEYnAVHiDlR-dyuU7hRjP6DyeTBVjPZzVuZwQ.webp" alt="" />
-            </div>
-            <div class="swiper-slide ${styles["swiper-slide"]}">
-            <img src="/img/xm68NH1fK_mqQM-BkfM4a0zO_e9eS_FBboFn57_8hhh5iD7Ey6gvdlTfnEDFBhrqzlGAsl-bNESVUTNwAc9fZRRquZKMiaFuujKeV8dqpV2S7zAahQ6FRb9WtRMDNA6qoSenyiYVU_WLEoWwLphCsA.webp" alt="" />
-          </div>
-          <div class="swiper-slide ${styles["swiper-slide"]}">
-          <img src="/img/4ooHzo_KLNYYrh5CMcZALkCglfXm7smmqX6yf_78Cpf81NjBQbyDPKMLFAGao3KmMhCPIswoU36Wf7dPhABfcsT5Ma_-x1hmifNFI-DerkUAgUS1qOsuUe5kn3kRMFqiZkUjpZ7RqvnwtfXQFNvmTw.webp" alt="" />
-        </div>
-        <div class="swiper-slide ${styles["swiper-slide"]}">
-        <img src="/img/1ebwxsys-XtwSBLp89zdH86rgHpAzX_kp9FXgUZfL8OQM3HmcU72WfirODZp_-lA7u5yTd9bS4YvzKJNaZfDoH-m_Nx_74d3c0LogCOfAhtnYzuwddUVUQZy4Ad8Kh1XH3OTs0SBZ1n9G95eEDt-MA.webp" alt="" />
-      </div>
-          </div>
-          <div class="swiper-pagination"></div>
-        </div>
-        </article>
+     
       </section>
     
     `
+
     setTimeout(function () {
-      new Swiper(".mySwiper", {
+      new Swiper(".swiper-container", {
         slidesPerView: 3,
-        spaceBetween: 30,
+        spaceBetween: 110,
         freeMode: true,
-        loop: true, // 반복 재생을 활성화
-        autoplay: {
-          delay: 1000, // 자동 재생 간격(밀리초 단위)
-          disableOnInteraction: true, // 사용자 상호작용 후에도 자동 재생 계속
-        },
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: false, // 페이지네이션 클릭 가능 여부
+        loop: true,
+        loopAdditionalSlides: 1,
+        autoplay: 3000,
+        autoplay: {  // 자동 슬라이드 설정 , 비 활성화 시 false
+          delay: 3000,   // 시간 설정
+          disableOnInteraction: false,  // false로 설정하면 스와이프 후 자동 재생이 비활성화 되지 않음
         },
       });
     }, 0);
